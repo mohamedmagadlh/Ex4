43c86386ab7d9a9ae1b35c7bc61c2a1899a0ce62
mohamedmagadlh <93886878+mohamedmagadlh@users.noreply.github.com>
2022-01-08 21:07:38 +0200

    Update GameGUI.java

--- gameClient/GameGUI.java
+++ gameClient/GameGUI.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class GameGUI extends JFrame {
 
     private Panel _panel;
@@ -16,13 +15,13 @@ public class GameGUI extends JFrame {
     private Controller _ctrl;
     private BufferedImage _image_pok;
 
-    public GameGUI(int level, Controller ctrl) {
+    public GameGUI( Controller ctrl) {
         super();
 
         _ctrl = ctrl;
         _panel = new Panel(this, _ctrl);
         _view = new GameViewPlus();
-        _panel.set_level(level);
+
 
         addWindowListener(_ctrl);
         Dimension user_dim = Toolkit.getDefaultToolkit().getScreenSize();
@@ -51,32 +50,14 @@ public class GameGUI extends JFrame {
         setIconImage(_image_pok);
     }
 
-
     private void menu() {
         MenuBar menuBar = new MenuBar();
-        Menu menu = new Menu("Level");
+        Menu menu = new Menu("stopgame");
         menuBar.add(menu);
         this.setMenuBar(menuBar);
         ArrayList<MenuItem> menuItems = new ArrayList<>();
-        String s = "Level number - ";
-        for (int i = 0; i < 24; i++) {
-            menuItems.add(new MenuItem(s + i));
+
         }
-        for (MenuItem i : menuItems) {
-            i.addActionListener(_ctrl);
-            menu.add(i);
-        }
-    }
-
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g);
-    }
-
-    public void set_level(int level) {
-        _panel.set_level(level);
-    }
-
     public void set_ar(Arena ar) {
         _view.set_ar(ar);
         _panel.set_ar(ar);
