8e71b8538f33bb7efff18dfcc917347afeb285de
mohamedmagadlh <93886878+mohamedmagadlh@users.noreply.github.com>
2022-01-14 20:16:36 +0200

    Update Range2Range.java

--- gameClient/util/Range2Range.java
+++ gameClient/util/Range2Range.java
@@ -4,26 +4,26 @@ import api.GeoLocation;
 
 
 public class Range2Range {
-	private Range2D _world, _frame;
+    private Range2D _world, _frame;
 
-	public Range2Range(Range2D w, Range2D f) {
-		_world = new Range2D(w);
-		_frame = new Range2D(f);
-	}
-	public GeoLocation world2frame(GeoLocation p) {
-		Point3D d = _world.getPortion(p);
-		Point3D ans = _frame.fromPortion(d);
-		return ans;
-	}
-	public GeoLocation frame2world(GeoLocation p) {
-		Point3D d = _frame.getPortion(p);
-		Point3D ans = _world.fromPortion(d);
-		return ans;
-	}
-	public Range2D getWorld() {
-		return _world;
-	}
-	public Range2D getFrame() {
-		return _frame;
-	}
+    public Range2Range(Range2D w, Range2D f) {
+        _world = new Range2D(w);
+        _frame = new Range2D(f);
+    }
+    public GeoLocation world2frame(GeoLocation p) {
+        point d = _world.getPortion(p);
+        point ans = _frame.fromPortion(d);
+        return ans;
+    }
+    public GeoLocation frame2world(GeoLocation p) {
+        point d = _frame.getPortion(p);
+        point ans = _world.fromPortion(d);
+        return ans;
+    }
+    public Range2D getWorld() {
+        return _world;
+    }
+    public Range2D getFrame() {
+        return _frame;
+    }
 }
